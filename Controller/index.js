3d77b83866509a3e49eeaa06630bb91e15b2d155
John <jegonzalezesparragoza@gmail.com>
2023-05-23 17:48:39 -0500

    feat: :sparkles: show info button
    botton albums shows a info button

--- Controller/index.js
+++ Controller/index.js
@@ -16,11 +16,14 @@ document.querySelector("#buscador").addEventListener("search",runApis)
 
 document.querySelector("#botones").addEventListener("click",(b)=>{
     let mainArea = document.querySelector("#mainArea");
+    let albumsArea = document.querySelector("#albumsArea");
     console.log(b.target.value);
     switch (b.target.value){
         case "Albums":
             mainArea.classList.remove("block-element");
-            mainArea.classList.add("hidden-element");              
+            mainArea.classList.add("hidden-element"); 
+            albumsArea.classList.remove("hidden-element");
+            albumsArea.classList.add("block-element")
             break
         case "Artists":
             console.log("Boton Artists");
@@ -35,6 +38,7 @@ document.querySelector("#botones").addEventListener("click",(b)=>{
             console.log("Boton All");
             mainArea.classList.remove("hidden-element");
             mainArea.classList.add("block-element");   
+            albumsArea.classList.remove("block-element")
     }
 })
 
