78a748cecc6eed449280d1239b262079e0fe0766
John González <jegonzalezesparragoza@gmail.com>
2023-05-23 10:38:14 -0500

    feat: :sparkles: Buttons Switch
    Will change the section with the buttons

--- Controller/index.js
+++ Controller/index.js
@@ -1,5 +1,6 @@
 import {doSearch} from "../Models/FirtsApi.js"
 import {showFirtsApi} from "../Models/showFirstApi.js"
+
 function createQueryString(string){
     let separada =string.split(" ")
     return separada.join("%20");
@@ -12,3 +13,24 @@ async function runApis(){
 }
 
 document.querySelector("#buscador").addEventListener("search",runApis)
+
+document.querySelector("#botones").addEventListener("click",(b)=>{
+    console.log(b.target.value);
+    switch (b.target.value){
+        case "Albums":
+            console.log("Boton Albums");
+            break
+        case "Artists":
+            console.log("Boton Artists");
+            break
+        case "Songs":
+            console.log("Boton Song");
+            break
+        case "Playlists":
+            console.log("Boton Playlist");
+            break
+        default:
+            console.log("Boton All");
+    }
+})
+
