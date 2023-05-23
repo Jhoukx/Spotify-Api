9c7c5ee83cf12bbf51af8f7e44629a9bacb8ac01
John González <jegonzalezesparragoza@gmail.com>
2023-05-22 22:47:46 -0500

    feat: :sparkles: TopArtist
    Show foto and name

--- Models/showFirstApi.js
+++ Models/showFirstApi.js
@@ -1,3 +1,8 @@
 export async function showFirtsApi (data){
+    //Foto y nombre Top Result
+    const topImageApi = data.topResults.items[0].data.visuals.avatarImage.sources[0].url
+    const topNameApi = data.topResults.items[0].data.profile.name
+    document.querySelector("#artist-image").src = `${topImageApi}`
+    document.querySelector("#artist-name").textContent = `${topNameApi}`
     
 }
